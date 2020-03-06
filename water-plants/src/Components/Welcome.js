49747ed5177426bbdf91c077a0be2d46ba260116
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-03-06 10:47:56 -0800

    add break

--- water-plants/src/Components/Welcome.js
+++ water-plants/src/Components/Welcome.js
@@ -4,6 +4,7 @@ export default function WelcomePage() {
     <section className="welcome-page">
       <header>
       <br />    
+      <br />
         <img className="image" src="https://images.unsplash.com/photo-1497990571654-77aa8ec36038?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2834&q=80" />
       </header>
     </section>
